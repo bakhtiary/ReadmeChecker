b16d7a68ef7e2a0913c0f87a9ec9786b3cc33873
amir bakhtiary davijani <amir.davijani.ext@adevinta.com>
2023-07-15 13:51:44 +0200

    find all md files

--- run_checks.py
+++ run_checks.py
@@ -1,1 +1,6 @@
 print("hello from python")
+import os
+from glob import glob
+result = [y for x in os.walk(".") for y in glob(os.path.join(x[0], '*.md'))]
+
+print(f"I found the following md files {result}")
